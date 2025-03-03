d17bf7418d2b8f29d56123fb9f810a81c71b6dfd
Andreyeyeye <shebanoff.andrei@yandex.ru>
2025-03-04 02:22:11 +0700

    почти ничего

---
+++ WpfApp1/PhoneBook.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1
+{
+	internal class PhoneBook
+	{
+		private Dictionary<string, string> _phones = new Dictionary<string, string>();
+		public PhoneBook(string pathFile) 
+		{
+			_phones = ReadPhonesFromFile(pathFile);
+		}
+
+		private Dictionary<string, string> ReadPhonesFromFile(string pathFile)
+		{
+			Dictionary<string, string> result = new Dictionary<string, string>();
+
+
+
+
+			return result;
+		}
+		private void WritePhonesToFile(string pathFile)
+		{
+			
+		}
+	}
+}
